53e38bda15e75f3f28d8bc5dd7f872a3e45eda31
ZacharyBaldock <93323069+ZacharyBaldock@users.noreply.github.com>
2022-02-18 15:44:33 +0400

    Add files via upload

---
+++ petstore1.webmanifest/petstore1.webmanifest.js
@@ -0,0 +1,15 @@
+{
+    "name": "My Pet Stor PWA",
+    "short_name": "petstore",
+    "Description", "my online petstore",
+    "Icons": [
+        {
+            "src": "images/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "start_url": "index.html",
+    "display": "fullscreen",
+    "background_colour": "#B12A34"
+}
